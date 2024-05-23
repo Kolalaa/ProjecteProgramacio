e06a5305d3d1bd1789afe357282889487dd94b41
Aaron <aaronmelladoberruezo@gmail.com>
2024-05-23 09:01:26 +0200

    arreglat sql

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module cat.boscdelacoma.sistemapersistent {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens cat.boscdelacoma.sistemapersistent to javafx.fxml;
     exports cat.boscdelacoma.sistemapersistent;

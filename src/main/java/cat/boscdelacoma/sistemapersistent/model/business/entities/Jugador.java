32964b9f33196042649e1212e16bf23de051bedc
storrent391 <storrent391@boscdelacoma.cat>
2024-05-24 09:55:56 +0200

    modificacions perque el sql funcioni

--- src/main/java/cat/boscdelacoma/sistemapersistent/model/business/entities/Jugador.java
+++ src/main/java/cat/boscdelacoma/sistemapersistent/model/business/entities/Jugador.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cat.boscdelacoma.sistemapersistent.model.business.entities;
 
-/**
- *
- * @author Sergi
- */
 public abstract class Jugador {
     private int id;
     private int idEquip;
@@ -27,33 +19,40 @@ public abstract class Jugador {
 
     public abstract void jugar();
 
-
     public int getId() {
         return id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public int getEdat() {
-        return edat;
-    }
-
-    public String getRol() {
-        return rol;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    public int getIdEquip() {
+        return idEquip;
+    }
+
+    public void setIdEquip(int idEquip) {
+        this.idEquip = idEquip;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    public int getEdat() {
+        return edat;
+    }
+
     public void setEdat(int edat) {
         this.edat = edat;
+    }
+
+    public String getRol() {
+        return rol;
     }
 
     public void setRol(String rol) {
@@ -64,24 +63,23 @@ public abstract class Jugador {
         return kills;
     }
 
-    public int getAssists() {
-        return assists;
-    }
-
-    public int getMorts() {
-        return morts;
-    }
-
     public void setKills(int kills) {
         this.kills = kills;
+    }
+
+    public int getAssists() {
+        return assists;
     }
 
     public void setAssists(int assists) {
         this.assists = assists;
     }
 
+    public int getMorts() {
+        return morts;
+    }
+
     public void setMorts(int morts) {
         this.morts = morts;
     }
-    
 }
--- src/main/java/cat/boscdelacoma/sistemapersistent/model/business/entities/Support.java
+++ src/main/java/cat/boscdelacoma/sistemapersistent/model/business/entities/Support.java
@@ -1,25 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cat.boscdelacoma.sistemapersistent.model.business.entities;
 
-
-
-/**
- *
- * @author Sergi
- */
 public class Support extends Jugador {
-    public Support (int id, String nom, int edat) {
+    public Support(int id, String nom, int edat, int kills, int assists, int morts) {
         super(id, nom, edat, "Support");
-        
+        setKills(kills);
+        setAssists(assists);
+        setMorts(morts);
     }
 
     @Override
     public void jugar() {
-        System.out.println("El support esta fent peel a l'ADC");
+        System.out.println("El support esta ajudant a l'equip.");
     }
-    
-    
 }
